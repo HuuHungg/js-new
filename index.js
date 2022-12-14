1d7589054df540919dd693a3ffd5e8dece2e93d3
HuuHungg <116856155+HuuHungg@users.noreply.github.com>
2022-12-14 12:34:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ let name = ['Huu Hung ', 'Ha Noi, VN']
 let [fullName, address] = name
 
 console.log('Chek key', fullName, address,)
+
+
+let state = {
+  name: 'Javascript',
+  course: 1000,
+  address: 'HN'
+  
+}
+
+let {name , course,} = state
